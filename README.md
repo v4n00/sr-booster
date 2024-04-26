6bf843b6c88f7a253f5f9ad5de084de91e79d387
v4n00 <105587619+v4n00@users.noreply.github.com>
2024-04-26 12:05:04 +0300

    updated to work for multiple players

--- README.md
+++ README.md
@@ -1,20 +1,20 @@
 # SR Booster
 
-This small application was created to simulate ranked matches in the game [SpeedRunners](https://store.steampowered.com/app/207140/SpeedRunners/) in order to gain points.
+This small application was created to simulate ranked matches in the game [SpeedRunners](https://store.steampowered.com/app/207140/SpeedRunners/) in order to gain points automatically.
 
 The application is meant to be ran on a Raspberry Pi.
 
-# Requirements
+## Requirements
 
--   two steam accounts that have SpeedRunners
--   a way to catch outgoing HTTP requests, like [Wireshark](https://www.wireshark.org/) or [Fiddler](https://www.telerik.com/download/fiddler)
--   a JavaScript runtime like [Node](https://nodejs.org/en)
+- two (or more) steam accounts that have SpeedRunners
+- a way to catch outgoing HTTP requests, like [Wireshark](https://www.wireshark.org/) or [Fiddler](https://www.telerik.com/download/fiddler) (recommended)
+- a JavaScript runtime like [Node](https://nodejs.org/en)
 
-# Instructions
+## Instructions
 
 1. open the [index.js](./index.js) file at the top where you can find the configuration variables
-2. get the [steam3ID's](https://steamrep.com/) (the number after the second `:`) of both accounts and place them in `p1id` and `p2id` (player 1 is the player who will win the matches)
+2. get the [steam3ID's](https://steamrep.com/) (the number after the second `:`) of the accounts and place them in `playerIds` array (first player is favored to win, based on `playerWinProbability` variable)
 3. launch your preffered network analyzer app
 4. launch SpeedRunners
-5. from any request that sends your `code` (like `GetRankingDetails`), grab the `code` and place it in `p1code` and `p2code` respectively
+5. from any request that sends your `code` (like `GetRankingDetails`), grab the `code` and place it in `playerTickets` array, make sure the order matches the `playerIds` array
 6. run `node index.js`
--- index.js
+++ index.js
@@ -1,119 +1,127 @@
-// ----- constants -----
-let APILink = 'http://league.speedrunners.doubledutchgames.com/';
-let pathGetRanking = 'Service/GetRanking';
-let pathCheckIn = 'Service/CheckIn';
-let pathCheckOut = 'Service/CheckOut';
 // ----- configuration -----
-let checkOutAverage = 90; // default: 90
-let cooldownAverage = 45; // default: 45
-let p1id = 0;
-let p2id = 0;
-let p1code = 0;
-let p2code = 0;
-// ----- variables -----
-let duration = 0;
-let cooldown = 0;
-let p1score = 3;
-let p2score = 0;
+const checkInCooldownAverage = 90; // default: 90, cooldown for how long the game lasts
+const checkOutCooldownAverage = 45; // default: 45, cooldown for in between games
+const playerIds = []; // place player IDs here, separated by commas, first player is the favored one
+const playerTickets = []; // place player tickets here, must be in the same order as player IDs
+const playerWinProbability = 95; // default: 95, win probability of first player
+// ----- constants & variables -----
+const APILink = 'http://league.speedrunners.doubledutchgames.com/';
+const pathGetRanking = 'Service/GetRanking';
+const pathCheckIn = 'Service/CheckIn';
+const pathCheckOut = 'Service/CheckOut';
+let checkInCooldown = 0;
+let checkOutCooldown = 0;
 let iterations = 0;
+let numberOfPlayers = 0;
+let playerScores = [];
 // ----- end -----
 
-async function main() {
-	log('Starting service in 15 seconds...');
+const main = async () => {
+	log('Starting service in 10 seconds...');
+
+	if (playerIds.length < 2 || playerIds.length > 4 || playerIds.length != playerTickets.length) {
+		log(`Invalid number of players (${playerIds.length}), must be >2 and <=4`);
+		process.exit(1);
+	} else {
+		numberOfPlayers = playerIds.length;
+		log(`Number of players: ${numberOfPlayers}`);
+	}
 
 	callGetRankingDetails();
-	setInterval(callGetRankingDetails, 600 * 1000);
-	await sleep(15);
+	setInterval(callGetRankingDetails, 10 * 60 * 1000);
+	await sleep(10);
 
 	while (true) {
 		iterations++;
-		duration = getRandom(checkOutAverage - 20, checkOutAverage + 20);
-		cooldown = getRandom(cooldownAverage - 10, cooldownAverage + 10);
+		checkInCooldown = getRandom(checkInCooldownAverage - 20, checkInCooldownAverage + 20);
+		checkOutCooldown = getRandom(checkOutCooldownAverage - 10, checkOutCooldownAverage + 10);
 		randomiseScores();
 
 		await callAPIfn(checkIn);
 		log(`Checked in  successfully (${iterations})`);
-		await sleep(duration);
+		await sleep(checkInCooldown);
 
 		await callAPIfn(checkOut);
 		log(`Checked out successfully (${iterations})`);
-		await sleep(cooldown);
+		await sleep(checkOutCooldown);
 	}
-}
+};
 
-async function callAPIfn(fn) {
+const callAPIfn = async (fn) => {
 	try {
 		return await fn();
 	} catch (e) {
 		errorHandler(e);
 	}
-}
+};
 
-async function callGetRankingDetails() {
-	log(`Player 1 rank: ${await getRankingDetails(p1code, p1id)}`);
-	log(`Player 2 rank: ${await getRankingDetails(p2code, p2id)}`);
-}
+const callGetRankingDetails = async () => {
+	for (let i = 0; i < numberOfPlayers; i++) {
+		log(`Player ${i + 1} rank: ${await getRankingDetails(playerTickets[i], playerIds[i])}`);
+	}
+};
 
-async function getRankingDetails(ticket, pid) {
-	let requestBody = `v=107&code=${ticket}&id=${pid}`;
+const getRankingDetails = async (playerTicket, playerId) => {
+	let requestBody = `v=107&code=${playerTicket}&id=${playerId}`;
 
 	try {
 		return (await post(pathGetRanking, requestBody)).data.score;
 	} catch (e) {
 		errorHandler(e);
 	}
-}
+};
 
-async function checkIn() {
-	let body1 = `v=107&code=${p1code}&id=${p1id}&pid[]=${p1id}&pid[]=${p2id}`;
-	let body2 = `v=107&code=${p2code}&id=${p2id}&pid[]=${p1id}&pid[]=${p2id}`;
+const checkIn = async () => {
+	for (let i = 0; i < numberOfPlayers; i++) {
+		let body = `v=107&code=${playerTickets[i]}&id=${playerIds[i]}`;
+		for (let j = 0; j < numberOfPlayers; j++) {
+			body += `&pid[]=${playerIds[j]}`;
+		}
+		await post(pathCheckIn, body);
+	}
+};
 
-	await post(pathCheckIn, body1);
-	await post(pathCheckIn, body2);
-}
+const checkOut = async () => {
+	for (let i = 0; i < numberOfPlayers; i++) {
+		let body = `v=107&code=${playerTickets[i]}&id=${playerIds[i]}`;
+		for (let j = 0; j < numberOfPlayers; j++) {
+			body += `&pscore[]=${playerScores[j]}`;
+		}
+		await post(pathCheckOut, body);
+	}
+};
 
-async function checkOut() {
-	let body1 = `v=107&code=${p1code}&id=${p1id}&pscore[]=${p1score}&pscore[]=${p2score}`;
-	let body2 = `v=107&code=${p2code}&id=${p2id}&pscore[]=${p1score}&pscore[]=${p2score}`;
-
-	await post(pathCheckOut, body1);
-	await post(pathCheckOut, body2);
-}
-
-async function post(path, body) {
+const post = async (path, body) => {
 	let headers = { headers: { 'Content-Length': body.length } };
 	return (await fetch(APILink + path, { method: 'POST', body: body, headers: headers })).json();
-}
+};
 
-async function errorHandler(e) {
+const errorHandler = async (e) => {
 	log(`Error occured: ${e.response.status || 0} - ${e.message}`);
 	if (e.response.status == 401) process.exit(1);
-}
+};
 
-function randomiseScores() {
-	if (getRandom(0, 101) < 95) {
-		p1score = 3;
-		p2score = getRandom(0, 3);
-	} else {
-		p1score = getRandom(0, 3);
-		p2score = 3;
+const randomiseScores = () => {
+	const winner = getRandom(0, 101) < playerWinProbability ? 0 : getRandom(1, numberOfPlayers);
+	for (let i = 0; i < numberOfPlayers; i++) {
+		playerScores[i] = i === winner ? 3 : getRandom(0, 3);
 	}
-}
+};
 
-async function sleep(seconds) {
+const sleep = async (seconds) => {
 	return new Promise((resolve) => setTimeout(resolve, seconds * 1000));
-}
+};
 
-function getRandom(min, max) {
+const getRandom = (min, max) => {
 	return Math.floor(Math.random() * (max - min)) + min;
-}
+};
 
-function log(message) {
+const log = (message) => {
 	const pad = (n) => (n < 10 ? '0' + n : n);
 	const now = new Date();
 	const logMessage = `[${pad(now.getHours())}:${pad(now.getMinutes())}:${pad(now.getSeconds())}] ${message}\n`;
 
 	process.stdout.write(logMessage);
-}
+};
 
 main();
